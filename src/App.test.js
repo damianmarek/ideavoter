29521ee1ca9ab4f60439289cef645c37c4ceab0f
Damian Marek <damianmateuszmarek@gmail.com>
2017-03-22 01:08:09 +0100

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import getStore from './redux'
 import App from './App';
+
+const app = (
+  <Provider store={getStore()}>
+    <App />
+  </Provider>
+)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(app, div);
 });
