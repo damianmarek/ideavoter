329f7ae6b881b5dd6fa4efc3ff3022c852ba2a1c
Damian Marek <damianmateuszmarek@gmail.com>
2017-03-22 02:16:09 +0100

    Add idea delete action

--- src/redux/ideasRedux.js
+++ src/redux/ideasRedux.js
@@ -2,7 +2,8 @@ import { createActions, createReducer } from 'reduxsauce'
 import Immutable from 'seamless-immutable'
 
 const { Types, Creators } = createActions({
-  addIdea: ['text']
+  addIdea: ['text', 'id'],
+  removeIdea: ['id'],
 })
 
 export const IdeasTypes = Types
@@ -13,8 +14,18 @@ export const INITIAL_STATE = Immutable({
 })
 
 export const addIdea = (state, action) =>
-  state.setIn(['list'], [...state.list, action.text])
+  state.setIn(['list'],
+    [...state.list,
+    {
+      text: action.text,
+      id: action.id
+    }
+  ])
+
+export const removeIdea = (state, action) =>
+  state.setIn(['list'], [...state.list].filter((obj) =>  obj.id !== action.id))
 
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.ADD_IDEA]: addIdea,
+  [Types.REMOVE_IDEA]: removeIdea,
 })
